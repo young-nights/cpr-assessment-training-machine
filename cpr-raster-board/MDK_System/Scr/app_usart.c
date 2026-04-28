b34a796c4aad03fbcc9d7afa347bd022be3d8d48
young-whites <1845243912@qq.com>
2026-04-28 18:20:57 +0800

    编译文件

--- cpr-raster-board/MDK_System/Scr/app_usart.c
+++ cpr-raster-board/MDK_System/Scr/app_usart.c
@@ -7,17 +7,15 @@ USART_ReceiveDataTypedef    USART1_QueueBuf;
 
 
 /**
-  * @brief  UART1 �������ú���
+  * @brief  UART1 
   * @param  None
   * @retval UART1-RX   -->  PA4
   *         UART1-TX   -->  PA5
   */
 void UART1_Config(void)
 {
-    /* ��λ���е� UART1 ��صĴ��ڼĴ��� */
     UART1_DeInit();
 
-    /* ���� UART1 ��ʱ�� */
     CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART1, ENABLE);
 
   /* UART1 configured as follow:
@@ -30,16 +28,12 @@ void UART1_Config(void)
   */
     UART1_Init(115200, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO, UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE); 
 
-    /* ʹ��UART1�����жϣ��ж�������Ϊ18 */
     UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE);
 
-    /* ʹ�� UART1 */
     UART1_Cmd(ENABLE);
     
 
-    /* ��ʼ�������������룬�����ж� */
     GPIO_Init(GPIOD, GPIO_PIN_6, GPIO_MODE_IN_PU_NO_IT);
-    /* ��ʼ����������������� */
     GPIO_Init(GPIOD, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST);
 
 }
@@ -49,12 +43,7 @@ void UART1_Config(void)
 
 
 
-/**
-  * @brief  UART1 ���ڷ�������
-  * @param  *buffer   Ҫ���͵�����
-  *         size      Ҫ���͵�����Ĵ�С
-  * @retval None
-  */
+
 void USART1_SendData(u8 * buffer , u16 size )
 {
     u16 i = 0;
@@ -76,12 +65,7 @@ void USART1_SendData(u8 * buffer , u16 size )
 
 
 
-/**
-  * @brief  UART1 ���ڻ��ζ���Ԫ�����ݳ�ʼ��
-  * @param  *Uart_Device_Rx   �������ݽṹ������
-  *         Length            ���ݳ���
-  * @retval None
-  */
+
 void    UART1_ReceiveValueInit(USART_ReceiveDataTypedef* Uart_Device_Rx, uint16_t Length)
 {
     Uart_Device_Rx->receive_last = Uart_Device_Rx->Receive_Buffer;
@@ -95,12 +79,7 @@ void    UART1_ReceiveValueInit(USART_ReceiveDataTypedef* Uart_Device_Rx, uint16_
 
 
 
-/**
-  * @brief  UART1 ���ڽ��պ���
-  * @param  *Uart_Device_Rx   �������ݽṹ������
-  *         Data              ��������
-  * @retval None
-  */
+
 void UART1_Receive(USART_ReceiveDataTypedef* Uart_Device_Rx, uint8_t Data)
 {
     if (!Uart_Device_Rx->receive_full_flag)
@@ -123,11 +102,6 @@ void UART1_Receive(USART_ReceiveDataTypedef* Uart_Device_Rx, uint8_t Data)
 
 
 
-/**
-  * @brief  UART1 ���ڻ�ȡһ���ֽ����ݺ���
-  * @param  *Uart_Device_Rx   �������ݽṹ������
-  * @retval None
-  */
 uint8_t UART1_GetByte(USART_ReceiveDataTypedef* Uart_Device_Rx)
 {
     uint8_t data = 0;
